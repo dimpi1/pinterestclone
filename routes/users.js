896eb630a12b42fc06b344fdae51f61749a0d52a
Dimpi <khandelwaldimpi6@gmail.com>
2024-05-16 06:36:39 +0530

    final commit

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/pin");
+mongoose.connect("mongodb+srv://dimpi1241be20:TJZi4JoZk7wCJIFs@cluster0.1tewfag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 const userSchema = mongoose.Schema({
   username : String,
